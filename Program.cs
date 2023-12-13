8bbd61720a685590f1ceadfdaddfbfc875559ee3
Lucas Bomfim <bmfmlucas@gmail.com>
2023-12-13 20:54:02 -0300

    Desafio Concluídogit add Program.csgit add Program.cs!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("SMARTPHONE NOKIA:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "5432111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SMARTPHONE IPHONE");
+Smartphone iphone = new Iphone(numero : "400235", modelo: "Modelo 2", imei: "6631341", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("TELEGRAM");
